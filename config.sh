032c8118c17ffba9bacd0d6370bdf88b8e5de9a4
Marcus Fredriksson <marcus@marcusfredriksson.com>
2024-01-28 23:12:42 +0100

    Added script to install directly from github

--- config.sh
+++ config.sh
@@ -8,5 +8,5 @@ DEV_IDE=nvim # editor, e.g. nvim, code, etc.
 DEV_BASEPATH="$HOME/Development" # where to store projects
 DEV_SHOULD_CLOSE=0 # 0=keep shell open, 1=close shell after IDE process exits
 
-START_DELIMITER="# ---start-dev---"
-END_DELIMITER="# ---end-dev---"
+START_DELIMITER="# ---START-DEV---"
+END_DELIMITER="# ---END-DEV---"
---
+++ install_from_cloud.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/marcusfrdk/dev.git /tmp/dev
+cd /tmp/dev
+./install.sh
+rm -rf /tmp/dev
